e84556cfc0c9d2b861b7b265af646cc6240a7f41
Nico <haeffnernico@gmail.com>
2018-01-26 16:23:54 +0100

    Initial Comit

---
+++ Enums/HexUnitType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerClientShare.Enums
+{
+    public enum HexUnitType
+    {
+        Builder,
+        Fighter
+    }
+}
+
+
---
+++ Enums/ResourceType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerClientShare.Enums
+{
+    public enum ResourceType
+    {
+        Wood,
+        Stone,
+        Glass
+    }
+}
