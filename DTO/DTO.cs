ba3b473ac7d97e2c7a2eab3b98e9fe9d7bcebc96
Nico <haeffnernico@gmail.com>
2018-01-30 11:37:29 +0100

    updates using statements

--- DTO/DTO.cs
+++ DTO/DTO.cs
@@ -4,7 +4,13 @@ using System.ComponentModel;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
+
+#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBGL || UNITY_IOS || UNITY_IPHONE || UNITY_ANDROID || UNITY_WII || UNITY_PS4 || UNITY_SAMSUNGTV || UNITY_XBOXONE || UNITY_TIZEN || UNITY_TVOS || UNITY_WP_8_1 || UNITY_WSA || UNITY_WSA_8_1 || UNITY_WSA_10_0 || UNITY_WINRT || UNITY_WINRT_8_1 || UNITY_WINRT_10_0
 using PlayerIO.GameLibrary;
+#else
+using PlayerIO.GameLibrary;
+#endif
+
 using ServerClientShare.Enums;
 //using Newtonsoft.Json;
 using ServerClientShare.MiniJson;
