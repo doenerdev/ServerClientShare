a7d8eb15fcef0c3f4a9e3bd457369d07aaacd57e
Nico <haeffnernico@gmail.com>
2018-02-19 18:54:50 +0100

    Moved Generation Services (HexMap, Deck etc.) to ServerClientShare

---
+++ Services/DeckService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServerClientShare.DTO;
+using ServerClientShare.Enums;
+using ServerClientShare.Helper;
+
+namespace ServerClientShare.Services
+{
+    public class DeckService
+    {
+        public const int DeckSize = 30;
+        public const int MarketplaceSize = 4;
+
+        private DeckDTO _deck;
+        private DeckDTO _marketplace;
+        private RandomGenerator _rndGenerator;
+
+        public DeckDTO Deck
+        {
+            get
+            {
+                if (_deck == null)
+                {
+                    Console.WriteLine("Generate Deck");
+                    _deck = GenerateDeck(DeckSize);
+                }
+                return _deck;
+            }
+        }
+
+        public DeckDTO Marketplace
+        {
+            get
+            {
+                if (_marketplace == null)
+                {
+                    Console.WriteLine("Generate Marketplace");
+                    _marketplace = GenerateDeck(MarketplaceSize);
+                }
+                return _marketplace;
+            }
+        }
+
+        public DeckService(RandomGenerator rndGenerator)
+        {
+            _rndGenerator = rndGenerator;
+        }
+
+        private DeckDTO GenerateDeck(int size)
+        {
+            var dto = new DeckDTO();
+            dto.DeckSize = size;
+
+            for (int i = 0; i < dto.DeckSize; i++)
+            {
+                CardType cardType = CardType.CreateBuilder;
+                var rnd = _rndGenerator.RandomRange(0D, 1D);
+                if (rnd > 0.65D)
+                {
+                    cardType = CardType.CreateBuilder;
+                }
+                else if (rnd > 0.3D)
+                {
+                    cardType = CardType.CreateFighter;
+                }
+                else
+                {
+                    cardType = CardType.Movement3;
+                }
+                dto.Cards.Add(new CardDTO() { CardType = cardType });
+            }
+
+            return dto;
+        }
+
+    }
+}
---
+++ Services/HexMapService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServerClientShare.DTO;
+using ServerGameCode.Services;
+
+namespace ServerClientShare.Services
+{
+    public class HexMapService
+    {
+        private HexCellService _hexCellService;
+        private HexMapDTO _currentHexMapDto;
+        private HexMapSize _mapSize;
+
+        public HexMapDTO CurrentHexMapDto => _currentHexMapDto ?? (_currentHexMapDto = GenerateNewHexMap(HexMapSize.L));
+
+        public HexMapService(HexCellService hexCellService, HexMapSize mapSize)
+        {
+            _hexCellService = hexCellService;
+            _mapSize = mapSize;
+        }
+
+        private HexMapDTO GenerateNewHexMap(HexMapSize size)
+        {
+            Console.WriteLine("Generate New Hex Map");
+            var dto = new HexMapDTO(size);
+            var cells = new List<HexCellDTO>();
+
+            for (int z = 0, i = 0; z < dto.Height; z++)
+            {
+                for (int x = 0; x < dto.Width; x++)
+                {
+                    cells.Add(_hexCellService.CreateHexCell(x, z, i++));
+                }
+            }
+            dto.Cells = cells;
+
+            return dto;
+        } 
+    }
+}
