5a5cfd9c263951e1b397f4210dce51bb4c5f5cf1
Nico <haeffnernico@gmail.com>
2018-03-03 16:04:22 +0100

    Updated Leader DTO

--- DTO/LeaderDTO.cs
+++ DTO/LeaderDTO.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ServerClientShare.Enums;
 #if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBGL || UNITY_IOS || UNITY_IPHONE || UNITY_ANDROID || UNITY_WII || UNITY_PS4 || UNITY_SAMSUNGTV || UNITY_XBOXONE || UNITY_TIZEN || UNITY_TVOS || UNITY_WP_8_1 || UNITY_WSA || UNITY_WSA_8_1 || UNITY_WSA_10_0 || UNITY_WINRT || UNITY_WINRT_8_1 || UNITY_WINRT_10_0
 using PlayerIOClient;
 #else
@@ -11,6 +12,7 @@ namespace ServerClientShare.DTO
 {
     public class LeaderDTO : DatabaseDTO<LeaderDTO>
     {
+        public LeaderType Type { get; set; }
         public string Name { get; set; }
         public LeaderSkillDTO PrimarySkill { get; set; }
         public List<ActiveLeaderSkillDTO> SecondarySkills { get; set; }
@@ -22,6 +24,7 @@ namespace ServerClientShare.DTO
 
         public override Message ToMessage(Message message)
         {
+            message.Add((int) Type);
             message.Add(Name);
             message = PrimarySkill.ToMessage(message);
             message.Add(SecondarySkills.Count);
@@ -37,6 +40,7 @@ namespace ServerClientShare.DTO
         public new static LeaderDTO FromMessageArguments(Message message, ref uint offset)
         {
             LeaderDTO dto = new LeaderDTO();
+            dto.Type = (LeaderType) message.GetInt(offset++);
             dto.Name = message.GetString(offset++);
             dto.PrimarySkill = LeaderSkillDTO.FromMessageArguments(message, ref offset);
 
@@ -52,6 +56,7 @@ namespace ServerClientShare.DTO
         public override DatabaseObject ToDBObject()
         {
             DatabaseObject dbObject = new DatabaseObject();
+            dbObject.Set("Type", (int) Type);
             dbObject.Set("Name", Name);
             dbObject.Set("PrimarySkill", PrimarySkill.ToDBObject());
             dbObject.Set("SecondarySkillsCount", SecondarySkills.Count);
@@ -72,6 +77,7 @@ namespace ServerClientShare.DTO
 
             LeaderDTO dto = new LeaderDTO
             {
+                Type = (LeaderType) dbObject.GetInt("Type"),
                 Name = dbObject.GetString("Name"),
                 PrimarySkill = LeaderSkillDTO.FromDBObject(dbObject.GetObject("PrimarySkill"))
             };
