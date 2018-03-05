d76f31e17d6082d8a7e7d3a3ee412e429334235d
Nico <haeffnernico@gmail.com>
2018-03-05 12:59:15 +0100

    Updated ActionLog

--- Models/PlayerActionsLog.cs
+++ Models/PlayerActionsLog.cs
@@ -77,6 +77,7 @@ namespace ServerGameCode
             return dbObject;
         }
 
+#if !(UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBGL || UNITY_IOS || UNITY_IPHONE || UNITY_ANDROID || UNITY_WII || UNITY_PS4 || UNITY_SAMSUNGTV || UNITY_XBOXONE || UNITY_TIZEN || UNITY_TVOS || UNITY_WP_8_1 || UNITY_WSA || UNITY_WSA_8_1 || UNITY_WSA_10_0 || UNITY_WINRT || UNITY_WINRT_8_1 || UNITY_WINRT_10_0)
         public static PlayerActionsLog FromDBObject(DatabaseObject dbObject, ServerCode server)
         {
             if (dbObject.Count == 0) return null;
@@ -91,5 +92,21 @@ namespace ServerGameCode
 
             return log;
         }
+#else
+            public static PlayerActionsLog FromDBObject(DatabaseObject dbObject)
+        {
+            if (dbObject.Count == 0) return null;
+
+            PlayerActionsLog log = new PlayerActionsLog();
+     
+            var actionsDB = dbObject.GetArray("Cards");
+            for (int i = 0; i < actionsDB.Count; i++)
+            {
+                log._playerActions.Add(PlayerAction.FromDBObject((DatabaseObject)actionsDB[i]));
+            }
+
+            return log;
+        }
+#endif
     }
 }
