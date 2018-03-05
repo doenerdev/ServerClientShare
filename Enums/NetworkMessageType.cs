8abe97ede40735fbb71ea117ac56fc9af8293013
Nico <haeffnernico@gmail.com>
2018-03-05 14:05:28 +0100

    Updated NetworkMessageTypes

--- Enums/NetworkMessageType.cs
+++ Enums/NetworkMessageType.cs
@@ -14,6 +14,7 @@ namespace ServerClientShare.Enums
         RequestDeck,
         RequestMarketplace,
         RequestNewTowerSegment,
+        RequestActionLog,
 
         GameActionPerformed,
         ChangeTurnPerformed,
