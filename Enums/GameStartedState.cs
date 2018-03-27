a3ea9672c49754de484c2a56906bc7003d5b149a
Nico <haeffnernico@gmail.com>
2018-03-27 14:13:56 +0200

    updated dtos

---
+++ Enums/GameStartedState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerClientShare.Enums
+{
+    public enum GameStartedState
+    {
+        Pending,
+        Started,
+        Ended
+    }
+}
