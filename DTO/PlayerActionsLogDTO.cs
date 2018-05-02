ebb346a0e8abd0791a415bd49c697aa7f1cd3838
Nico <haeffnernico@gmail.com>
2018-05-02 16:22:09 +0200

    updated actionlogdto

--- DTO/PlayerActionsLogDTO.cs
+++ DTO/PlayerActionsLogDTO.cs
@@ -16,17 +16,14 @@ using ServerClientShare.Models;
 public class PlayerActionsLogDTO : DatabaseDTO<PlayerActionsLogDTO>
 {
     public List<PlayerAction> PlayerActions { get; set; }
-    public string PlayerName { get; set; }
 
-    public PlayerActionsLogDTO(string playerName)
+    public PlayerActionsLogDTO()
     {
         PlayerActions = new List<PlayerAction>();
-        PlayerName = playerName;
     }
 
     public override Message ToMessage(Message message)
     {
-        message.Add(PlayerName);
         message.Add(PlayerActions.Count);
 
         foreach (var playerAction in PlayerActions)
@@ -39,8 +36,7 @@ public class PlayerActionsLogDTO : DatabaseDTO<PlayerActionsLogDTO>
 
     public static PlayerActionsLogDTO FromMessageArguments(Message message, ref uint offset)
     {
-        var playerName = message.GetString(offset++);
-        var dto = new PlayerActionsLogDTO(playerName);
+        var dto = new PlayerActionsLogDTO();
         var actionsCount = message.GetInt(offset++);
 
         for (int i = 0; i < actionsCount; i++)
@@ -53,7 +49,6 @@ public class PlayerActionsLogDTO : DatabaseDTO<PlayerActionsLogDTO>
     public override DatabaseObject ToDBObject()
     {
         DatabaseObject dbObject = new DatabaseObject();
-        dbObject.Set("PlayerName", PlayerName);
 
         DatabaseArray actionsDB = new DatabaseArray();
         if (PlayerActions != null)
@@ -72,8 +67,7 @@ public class PlayerActionsLogDTO : DatabaseDTO<PlayerActionsLogDTO>
     {
         if (dbObject.Count == 0) return null;
 
-        var playerName = dbObject.GetString("PlayerName");
-        var dto = new PlayerActionsLogDTO(playerName);
+        var dto = new PlayerActionsLogDTO();
         var actionsDB = dbObject.GetArray("PlayerActions");
         for (int i = 0; i < actionsDB.Count; i++)
         {
